ae71661d3d8ee4831213c6ae52d4437dbd501b29
fadilparves <fadil.parves@gmail.com>
2020-09-22 16:12:51 +0800

    stochgrad class created with init method

--- stoch_gradient_descent.py
+++ stoch_gradient_descent.py
@@ -7,3 +7,12 @@ def rmse(prediction, truth):
     prediction = prediction[truth.nonzero()].flatten()
     truth = truth[truth.nonzero()].flatten()
     return sqrt(mean_squared_error(prediction, truth))
+
+class StochasticGradientDescent:
+    def __init__(self, n_epochs=200, n_latent_features=3, lmbda=0.1, learning_rate=0.001):
+        self.n_epochs = n_epochs
+        self.n_latent_features = n_latent_features
+        self.lmbda = lmbda
+        self.learning_rate = learning_rate
+
+    
